2d7889077a3f9d7153a22356c7f1b7730e0c7a6b
Juan Luna <Juan Luna@JUAN-ESCRITORIO>
2018-06-06 20:39:46 -0300

    primer commit

---
+++ apiPrueba/Controllers/pruebaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace apiPrueba.Controllers
+{
+    public class pruebaController : ApiController
+    {
+        
+        [HttpGet]
+        public IHttpActionResult algo()
+        {
+            X509Certificate2 cert = Request.GetClientCertificate();
+            string issuer = cert.Issuer;
+            string subject = cert.Subject;
+            return Ok("algo");
+        }
+    }
+}
